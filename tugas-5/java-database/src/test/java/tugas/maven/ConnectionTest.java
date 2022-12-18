8127700dba68a226beb8bace946781738914446a
Risa Meilenia <101375696+ris4meileni4@users.noreply.github.com>
2022-12-18 12:53:35 +0800

    Add files via upload
    TUGAS MAVEN

---
+++ tugas-5/java-database/src/test/java/tugas/maven/ConnectionTest.java
@@ -0,0 +1,38 @@
+package tugas.maven;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        try {
+            Driver mysqlDriver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(mysqlDriver);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Test
+    void testConnection() {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/java_database";
+        String username = "root";
+        String password = "";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl,username,password);
+
+            System.out.println("Koneksi Database Sukses!");
+        }catch (SQLException exception) {
+            Assertions.fail(exception);
+        }
+    }
+}
+
---
+++ tugas-5/java_database.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Waktu pembuatan: 18 Des 2022 pada 04.47
+-- Versi server: 5.7.36
+-- Versi PHP: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `java_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_tes`
+--
+
+DROP TABLE IF EXISTS `tbl_tes`;
+CREATE TABLE IF NOT EXISTS `tbl_tes` (
+  `id_tes` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_tes` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_tes`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
